c011fbd6eff463779f040e4905e4e1c3452475eb
Danil Nepomnyashih <nepomnachihd@gmail.com>
2020-12-06 13:05:57 +0600

    Change the build folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     output: {
         filename: '[name].[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     devServer: {
         port: 3000,
